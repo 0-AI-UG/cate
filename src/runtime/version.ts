df6570bce369e37ebf1364be4f57e2f228d0f088
Anton <antonhorn@icloud.com>
2026-08-13 22:09:43 +0800

    Release 1.6.1-beta.1

--- src/runtime/version.ts
+++ src/runtime/version.ts
@@ -6,4 +6,4 @@
 // matching runtime tarballs (see runtimeArtifacts.ts).
 // =============================================================================
 
-export const RUNTIME_VERSION = '1.6.0'
+export const RUNTIME_VERSION = '1.6.1-beta.1'
